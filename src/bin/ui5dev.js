17d9f232b6b208dc34cce032a966c0c9f3d92e2a
Tomasz Krzyszczyk <tomasz.krzyszczyk@trw.com>
2017-07-12 12:50:55 +0200

    Add `devserver` command

--- src/bin/ui5dev.js
+++ src/bin/ui5dev.js
@@ -6,12 +6,11 @@ import clean from '../commands/clean';
 import build from '../commands/build';
 import serve from '../commands/serve';
 
-import { generatePortNumber } from '../utils';
-
 
 const version = require('../../package.json').version;
-const src = path.join(process.cwd(), 'webapp');
-const dest = path.join(process.cwd(), 'dist');
+const cwd = process.cwd();
+const src = path.join(cwd, 'webapp');
+const dest = path.join(cwd, 'dist');
 
 program
   .command('clean')
@@ -23,21 +22,23 @@ program
   .command('build')
   .action(function() {
     clean(dest).then(() => {
-      build(src, dest, {watch: true});
+      build(src, dest, {watch: false});
     });
   });
 
 program
   .command('serve')
   .action(function() {
-    const port = generatePortNumber();
-    serve(dest, port);
+    serve(dest);
   });
 
 program
-  .command('start')
+  .command('devserver')
   .action(function(args, next) {
-    console.log(`Running ${args} from ${process.cwd()}`);
+    clean(dest).then(() => {
+      build(src, dest, {watch: true});
+      serve(dest);
+    });
   });
 
 program
--- src/commands/serve.js
+++ src/commands/serve.js
@@ -1,13 +1,50 @@
+import url from 'url';
+import path from 'path';
 import express from 'express';
 import morgan from 'morgan';
 import chalk from 'chalk';
-import { log } from '../utils';
+import proxy from 'express-http-proxy';
+import saplogon from 'saplogon-read';
+import { log, generatePortNumber } from '../utils';
 
 
-function serve(dest, port) {
+function getDestinations(dest) {
+  const routes = require(path.join(path.dirname(dest), 'neo-app.json')).routes;
+  return routes.map(function(route) {
+    if (route.target.type === 'destination') {
+      const system = route.target.name;
+      const logon = saplogon(system);
+      return {
+        system,
+        path: route.path,
+        host: logon.host,
+        https: false,
+      };
+    }
+
+    return {
+      path: route.path,
+      host: 'sapui5.hana.ondemand.com',
+      https: true,
+    }
+  });
+}
+
+
+function serve(dest) {
   const app = express();
+  const port = generatePortNumber();
+  const destinations = getDestinations(dest);
+
   app.use(morgan('short'));
   app.use(express.static(dest));
+
+  destinations.forEach(function(destination) {
+    app.use(destination.path, proxy(destination.host, {
+      proxyReqPathResolver: req => destination.path + url.parse(req.url).path,
+      https: destination.https,
+    }));
+  });
 
   const server = app.listen(port, '0.0.0.0', function() {
     const host = `http://127.0.0.1:${server.address().port}`;
