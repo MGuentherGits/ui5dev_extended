e0654ccc8acce75a49b1793d872032f818e80306
Tomasz Krzyszczyk <tomasz.krzyszczyk@trw.com>
2017-07-25 16:04:24 +0200

    better log messages

--- src/commands/serve.js
+++ src/commands/serve.js
@@ -1,33 +1,61 @@
 import url from 'url';
 import path from 'path';
+import fs from 'fs';
 import express from 'express';
 import morgan from 'morgan';
 import chalk from 'chalk';
 import proxy from 'express-http-proxy';
 import saplogon from 'saplogon-read';
+import opn from 'opn';
 import { log, generatePortNumber } from '../utils';
 
 
 function getDestinations(dest) {
-  const routes = require(path.join(path.dirname(dest), 'neo-app.json')).routes;
-  return routes.map(function(route) {
-    if (route.target.type === 'destination') {
-      const system = route.target.name;
-      const logon = saplogon(system);
-      return {
-        system,
-        path: route.path,
-        host: logon.host,
-        https: false,
-      };
+  const cwd = path.dirname(dest);
+  let destinations = [];
+  
+  /* load config from neo-app.json */
+  try {
+    const configFile = path.join(cwd, 'neo-app.json');
+    if (fs.existsSync(configFile)) {
+      let routes = require(configFile).routes;
+      routes = routes.filter(r => r.target.type === 'destination').map(route => {
+        return {
+          path: route.path,
+          system: route.target.name
+        }
+      });
+      destinations = destinations.concat(routes);
     }
+  } catch (ex) {
+    log(`${chalk.red('Error parsing neo-app.json file.')}`);
+  }
 
-    return {
-      path: route.path,
-      host: 'sapui5.hana.ondemand.com',
-      https: true,
+  /* load config from config.json */
+  try {
+    const configFile = path.join(cwd, 'config.json');
+    if (fs.existsSync(configFile)) {
+      let routes = require(configFile).routes;
+      destinations = destinations.concat(routes);
     }
+  } catch (ex) {
+    log(`${chalk.red('Error parsing config.json file.')}`);
+  }
+
+  destinations = destinations.map(function(route) {
+    const logon = saplogon(route.system);
+    if (!logon) {
+      log(`${chalk.red('Error')}: Unknown system ${chalk.cyan(route.system)} for ${chalk.yellow(route.path)}`);
+      return null;
+    }
+    return {
+      system: route.system,
+      path: route.path,
+      host: logon.host,
+      https: false,
+    };
   });
+  return destinations.filter(destination => destination !== null);
 }
 
 
@@ -48,7 +76,16 @@ function serve(dest) {
 
   const server = app.listen(port, '0.0.0.0', function() {
     const host = `http://127.0.0.1:${server.address().port}`;
+    opn(host);
+
     log(`Development server listening on ${chalk.yellow.underline(host)}`);
+    if (destinations.length > 0) {
+      destinations.forEach(destination => {
+        log(`destination: ${chalk.yellow(destination.path)} => ${chalk.cyan(destination.host)} (${destination.system})`);
+      });
+    } else {
+      log(`No external destinations loaded.`);
+    }
   });
 }
 
